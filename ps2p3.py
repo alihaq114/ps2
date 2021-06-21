81db5b9f9c25528cea8ab794e08603835ce691ed
alihaq114 <85802743+alihaq114@users.noreply.github.com>
2021-06-20 20:30:51 -0400

    Add files via upload
    both the ps2p3 file's code and flowchart

---
+++ ps2p3.py
@@ -0,0 +1,13 @@
+print(enter number of books order)
+bo = float(input())
+print(enter cost per book)
+cpb = float(input())
+total = bo  cpb
+if total = 50.0
+    shp = 25
+else
+    shp = 0
+nt = total + shp
+print(your order =  + str(total))
+print(your shipping cost =  + str(shp))
+print(your total cost + shipping =  + str(nt))
